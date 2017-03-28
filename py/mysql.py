6e2e164d3714ec1d2dc18e68a9c97f1fd2600091
wangfeng <jkwangfeng@qq.com>
2017-03-28 18:19:43 +0800

    insert

--- py/mysql.py
+++ py/mysql.py
@@ -6,10 +6,12 @@ loop = asyncio.get_event_loop()
 @asyncio.coroutine
 def test_example():
     conn = yield from aiomysql.connect(host='127.0.0.1', port=3306,
-                                       user='root', password='', db='test',charset='utf8',
+                                       user='root', password='', db='test',
+                                       charset='utf8',autocommit=True,
                                        loop=loop)
 
     cur = yield from conn.cursor()
+    yield from cur.execute('insert into users (name) values ("你好")')   
     yield from cur.execute("SELECT id,name FROM users")
     print(cur.description)
     r = yield from cur.fetchall()
