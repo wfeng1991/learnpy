c49ae4ace11ad4df9065b7f763cce89fc4ca0eac
wangfeng <jkwangfeng@qq.com>
2017-03-29 14:38:24 +0800

    app init

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # learnpy
 根据廖雪峰的python教程
-练习python
+练习python web
---
+++ web/app.py
@@ -0,0 +1,22 @@
+import logging,asyncio
+from aiohttp import web
+
+
+logging.basicConfig(level=logging.INFO)
+
+def index():
+    return web.Response(body='Awesome')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
+
+
