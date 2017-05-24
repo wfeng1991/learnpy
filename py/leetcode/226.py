755fe42f6cbf19948ab4921f4e5b5c5f4471f9e5
wangfeng <jkwangfeng@qq.com>
2017-05-24 18:34:38 +0800

    leetcode 226

---
+++ py/leetcode/226.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root is None:
+            return root
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        t = root.left
+        root.left = root.right
+        root.right = t
+        return root
+        
+        
