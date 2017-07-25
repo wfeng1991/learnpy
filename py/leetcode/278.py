bad29b6e7941ddd8dc8e184acb3da85dec525cdc
默念 <wfeng@wf-mbp.local>
2017-07-25 12:31:36 +0800

    leetcode

---
+++ py/leetcode/278.py
@@ -0,0 +1,20 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low=1
+        high=n
+        while low<high:
+            mid=(low+high)//2
+            if isBadVersion(mid):
+                high=mid-1
+            else:
+                low=mid+1
+        return high if isBadVersion(high) else high+1
