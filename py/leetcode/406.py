643eb4abc4adf701c8837fb0f5ab981de1165bbb
默念 <wfeng@wf-mbp.local>
2017-07-31 16:44:24 +0800

    leetcode

--- py/leetcode/406.py
+++ py/leetcode/406.py
@@ -1,4 +1,9 @@
 class Solution(object):
+    '''
+    Below is my explanation of the following neat solution 
+    where we sort people from tall to short (and by increasing k-value) 
+    and then just insert them into the queue using their k-value as the queue index
+    '''
     def reconstructQueue(self, people):
         """
         :type people: List[List[int]]
