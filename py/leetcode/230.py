7fff484f3c2baddf7e4773eac40908a59babfb43
默念 <wfeng@wf-mbp.local>
2017-08-22 15:12:39 +0800

    leetcode

---
+++ py/leetcode/230.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        self.n=k
+        self.r=0
+        def help(root):
+            if root:
+                help(root.left)
+                self.n-=1
+                if self.n==0:
+                    self.r=root.val
+                help(root.right)
+        
+        
