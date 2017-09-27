b73bf126c83fedb9c6f2ca34171304f1a9908cc0
wangfeng <jkwangfeng@qq.com>
2017-09-27 16:55:12 +0800

    leetcode

--- py/leetcode/334.py
+++ py/leetcode/334.py
@@ -6,9 +6,9 @@ class Solution(object):
         """
         m1=m2=float('inf')
         for x in nums:
-            if x<m1:
+            if x<=m1:
                 m1=x
-            elif x<m2:
+            elif x<=m2:
                 m2=x
             else:
                 return True
