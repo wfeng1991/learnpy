0bfd6b14e874f28d5a7422dd6258afc98419ff7c
wangfeng <jkwangfeng@qq.com>
2017-09-27 16:54:18 +0800

    leetcode

---
+++ py/leetcode/334.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        m1=m2=float('inf')
+        for x in nums:
+            if x<m1:
+                m1=x
+            elif x<m2:
+                m2=x
+            else:
+                return True
+        return False
