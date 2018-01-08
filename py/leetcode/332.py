53bc189840e43f42c91fedc86ca1da380f38ec73
wfeng <wangfeng@motie.com>
2018-01-08 15:29:15 +0800

    leetcode

---
+++ py/leetcode/332.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findItinerary(self, tickets):
+        """
+        :type tickets: List[List[str]]
+        :rtype: List[str]
+        """
+        import collections
+        targets = collections.defaultdict(list)
+        for a, b in sorted(tickets)[::-1]:
+            targets[a] += b,
+        route = []
+        def visit(airport):
+            while targets[airport]:
+                visit(targets[airport].pop())
+            route.append(airport)
+        visit('JFK')
+        return route[::-1]
+
+print(Solution().findItinerary([["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]))
