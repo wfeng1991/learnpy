e8f45136609aac7ed3c16c93d996db311d4cd4c2
wfeng <wangfeng@motie.com>
2018-01-18 17:02:00 +0800

    leetcode

---
+++ py/leetcode/31.py
@@ -0,0 +1,23 @@
+class Solution:
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if len(nums)<2:
+            return
+        i=len(nums)-1
+        while i>0 and nums[i-1]>=nums[i]:
+            i-=1
+        if i==0:
+            nums=sorted(nums)
+        else:
+            j=i-1
+            p=len(nums)-1
+            while p>j and nums[p]<=nums[j]:
+                p-=1
+            nums[j],nums[p]=nums[p],nums[j]
+            nums[i:]=sorted(nums[i:])
+        print(nums)
+
+Solution().nextPermutation([3,2,1])
